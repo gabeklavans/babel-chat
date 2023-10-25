332f3967f046d6702b071b07511ee57de6ace4fa
gabe <gabeklav@fastmail.com>
2023-10-25 13:03:27 -0400

    get basic local tcp connection working

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    if let Ok(mut stream) = TcpStream::connect("localhost:9000") {
+        println!("Connected!");
+        let bytes_written = stream.write(b"world from rust!\n")?;
+        println!("bytes written: {}", bytes_written);
+        let mut buffer = [0; 128];
+        let bytes_read = stream.read(&mut buffer[..])?;
+        println!("bytes read: {}", bytes_read);
+        println!("Received: {:?}", String::from_utf8_lossy(&buffer));
+    } else {
+        panic!("Not conntected...");
+    }
+
+    Ok(())
 }
