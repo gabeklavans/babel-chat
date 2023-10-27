603dd9079c6cf28a54cbdaef7854659f85d54ada
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2023-10-27 17:13:15 -0400

    clean(zig): remove prints

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -17,16 +17,12 @@ test "simple test" {
     };
     defer stream.close();
 
-    // std.debug.print("\naddress: {}\n", .{address});
-
     const write_message = "You smell sorry!";
-    // std.debug.print("Write: {s}\n", .{write_message});
     const write_len = try stream.write(write_message);
     try std.testing.expectEqual(write_message.len, write_len);
 
     var read_message = try allocator.alloc(u8, write_message.len);
     const read_len = try stream.read(read_message);
 
-    // std.debug.print("Read: {s}\n", .{read_message[0..read_len]});
     try std.testing.expectEqualSlices(u8, write_message, read_message[0..read_len]);
 }
